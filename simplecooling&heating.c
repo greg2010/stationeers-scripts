e5a342fb3f6f1191dfe3962603e42d966c79abbc
triggers0 <45500221+triggers0@users.noreply.github.com>
2023-05-18 21:17:18 +0200

    Add files via upload
    Simple mips to activate a cooling device (digital valve) and heater based off input of two dials set to desired temperature in celsius.

---
+++ simplecooling&heating.c
@@ -0,0 +1,25 @@
+#define gassensor d0
+#define cooler d1
+#define heater d2
+#define dialmin d3
+#define dialmax d4
+
+num main(void) {
+    num isHot;
+    while (1) {
+        if ((load(gassensor, "Temperature") - 273) >= load(dialmax, "Setting")) {
+            store(heater, "On", 0);
+            store(cooler, "On", 1);
+        } 
+        if ((load(gassensor, "Temperature") - 273) <= load(dialmin, "Setting")) {
+            store(cooler, "On", 0);
+            store(heater, "On", 1);
+        }
+        else {
+            store(cooler, "On", 0);
+            store(heater, "On", 0);
+        }
+        sleep(5);
+    }    
+}
+num main(void)
