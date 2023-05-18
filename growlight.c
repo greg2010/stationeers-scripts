b185b03cee8e42eed4a87465980aa463e303a1ce
greg2010 <greg2008200@gmail.com>
2023-05-18 00:20:27 -0400

    init commit

---
+++ growlight.c
@@ -0,0 +1,15 @@
+#define sensor d0
+#define grow_light d1
+
+num main(void) {
+	num hasSun;
+	while (1) {
+		if (load(sensor, "Activate") == 1) {
+            store_batch(grow_light, "On", 1);
+		} else {
+            store_batch(grow_light, "On", 0);
+		}
+
+		yield();
+	}	
+}
