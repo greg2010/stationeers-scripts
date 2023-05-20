0514115acb528194aa5f80a9f525db354e37d3ef
greg2010 <greg2008200@gmail.com>
2023-05-20 19:26:10 -0400

    lights_control.c

---
+++ lights_control.c
@@ -0,0 +1,31 @@
+#define diode d0
+#define light d1
+
+num main(void) {
+    num diodeSumPrev;
+    num diodeSum;
+
+    store_batch(hash("StructureDiodeSlide"), "On", 0);
+    store_batch(hash("StructureLightRound"), "On", 1);
+    diodeSumPrev = load_batch(hash("StructureDiodeSlide"), "On", 1);
+    yield();
+
+    while (1) {
+        diodeSum = load_batch(hash("StructureDiodeSlide"), "On", 1);
+
+        if (diodeSum < diodeSumPrev)
+        {
+            store_batch(hash("StructureDiodeSlide"), "On", 0);
+            store_batch(hash("StructureLightRound"), "On", 1);
+            diodeSumPrev = diodeSum;
+        }
+         if ((diodeSum > diodeSumPrev))
+        {
+            store_batch(hash("StructureDiodeSlide"), "On", 1);
+            store_batch(hash("StructureLightRound"), "On", 0);
+            diodeSumPrev = diodeSum;
+        }
+
+        yield();
+    }
+}
